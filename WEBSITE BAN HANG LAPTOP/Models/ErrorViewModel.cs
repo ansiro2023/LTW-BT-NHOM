4e17dd0f02ee2ba132f9f3157d3fbee29a2831ea
Do Quang Vinh <2033210603@hufi.edu.vn>
2024-03-24 16:52:46 +0700

    TAO MOI 1 PROJECT .NET MVC XAY DUNG WEBSITE BAN LAPTOP

---
+++ WEBSITE BAN HANG LAPTOP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEBSITE_BAN_HANG_LAPTOP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
