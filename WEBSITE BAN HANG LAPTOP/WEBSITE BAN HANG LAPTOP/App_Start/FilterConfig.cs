1dc99c16c1e4bd8d5243311c8a9bbf55e1208c89
Do Quang Vinh <2033210603@hufi.edu.vn>
2024-03-24 19:45:36 +0700

    ADD NEW SOLUTION FRAMEWORK .NET 4.5: - MOI NGUOI LAY VE DE CODE TREN DAY

---
+++ WEBSITE BAN HANG LAPTOP/WEBSITE BAN HANG LAPTOP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WEBSITE_BAN_HANG_LAPTOP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WEBSITE BAN HANG LAPTOP/WEBSITE BAN HANG LAPTOP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEBSITE_BAN_HANG_LAPTOP.MvcApplication" Language="C#" %>
