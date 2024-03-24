34da44b6b51fd4c4a10dce86edfabd2c75e3137d
Do Quang Vinh <2033210603@hufi.edu.vn>
2024-03-24 19:02:54 +0700

    XOA PROJECT FRAMEWORK 6.0

--- WEBSITE BAN HANG LAPTOP/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WEBSITE_BAN_HANG_LAPTOP.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
